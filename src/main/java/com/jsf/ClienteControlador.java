d11f07a8c3b1553b87ca7f40d2606a50f47b1baa
Eduardo Montenegro <eduardomontenegro599@gmail.com>
2021-07-12 10:00:11 -0500

    lista vista del cliente y arreglo de la peticion, ya actualiza al borrar

--- src/main/java/com/jsf/ClienteControlador.java
+++ src/main/java/com/jsf/ClienteControlador.java
@@ -5,8 +5,8 @@
  */
 package com.jsf;
 
-import com.modelo.ProveedorMod;
-import com.objetos.Proveedor;
+import com.modelo.ClienteMod;
+import com.objetos.Cliente;
 import java.io.Serializable;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
@@ -17,70 +17,74 @@ import javax.faces.bean.SessionScoped;
  * @author panchito
  */
 @SuppressWarnings("deprecation")
-@ManagedBean(name = "proveedor")
+@ManagedBean(name = "cliente")
 @SessionScoped
-public class ProveedorControlador implements Serializable {
+public class ClienteControlador implements Serializable {
 
-	private static final long serialVersionUID = -8404778928087984105L;
+	private static final long serialVersionUID = -1113557213370916646L;
 	private String buscador;
-	private List<Proveedor> lista;
-	private Proveedor prov = new Proveedor();
-	private final ProveedorMod modprov = new ProveedorMod();
+	private List<Cliente> lista;
+	private Cliente cli = new Cliente();
+	private final ClienteMod modcli = new ClienteMod();
 
 	public void todo() {
-		this.setLista(modprov.todos());
+		this.setLista(modcli.todos());
 	}
 
 	public void buscar() {
-		this.setLista(this.modprov.buscando(buscador));
+		if (buscador.isEmpty()) {
+			this.todo();
+		} else {
+			this.setLista(this.modcli.buscando(buscador));
+		}
 	}
 
-	public void seleccionar(Proveedor pro) {
-		this.prov = pro;
+	public void seleccionar(Cliente cli) {
+		this.cli = cli;
 	}
 
 	public void borrar(int id) {
 		try {
-			if(this.modprov.borrar(id, UtilitarioControlador.getUsu().getUsu_id())){
+			if (this.modcli.borrar(id, UtilitarioControlador.getUsu().getUsu_id())) {
 				UtilitarioControlador.informativo("Borrado con Exito");
 				this.todo();
-			}else{
+			} else {
 				UtilitarioControlador.advertencia("Existio un problema al eliminar");
 			}
 		} catch (Exception e) {
-			UtilitarioControlador.error("Existio un error al eliminar: "+e.getMessage());
+			UtilitarioControlador.error("Existio un error al eliminar: " + e.getMessage());
 		}
 	}
-	
-	public void editar(){
-		this.prov.setUsu_id_UltMod(UtilitarioControlador.getUsu().getUsu_id());
+
+	public void editar() {
+		this.cli.setUsu_id_UltMod(UtilitarioControlador.getUsu().getUsu_id());
 		try {
-			if (this.modprov.actualizar(this.prov)) {
-				UtilitarioControlador.informativo("Proveedor Actualizado con exito");
+			if (this.modcli.actualizar(this.cli)) {
+				UtilitarioControlador.informativo("Cliente Actualizado con exito");
 				this.limpiar();
-			}else{
+			} else {
 				UtilitarioControlador.advertencia("Existio un problema al actualizar");
 				this.limpiar();
 			}
 		} catch (Exception e) {
-			UtilitarioControlador.error("Ocurrio un error al editar el proveedor"+e.getMessage());
+			UtilitarioControlador.error("Ocurrio un error al editar el cliente" + e.getMessage());
 		}
 	}
 
 	public void limpiar() {
 		this.todo();
-		this.setProv(new Proveedor());
+		this.setCli(new Cliente());
 	}
 
 	public void guardar() {
-		this.prov.setUsu_id_UltMod(UtilitarioControlador.getUsu().getUsu_id());
+		this.cli.setUsu_id_UltMod(UtilitarioControlador.getUsu().getUsu_id());
 		try {
-			if (this.prov.hasEmptyFields()) {
+			if (this.cli.hasEmptyFields()) {
 				UtilitarioControlador.advertencia("Existen Campos Vacios");
 			} else {
-				if (this.prov.getPais().equals("ecuador") || this.prov.getPais().equals("Ecuador")) {
-					if (UtilitarioControlador.validaRuc(this.prov.getRuc())) {
-						if (this.modprov.guardar(this.prov)) {
+				if (this.cli.getPais().equals("ecuador") || this.cli.getPais().equals("Ecuador")) {
+					if (UtilitarioControlador.validaRuc(this.cli.getRuc())) {
+						if (this.modcli.guardar(this.cli)) {
 							UtilitarioControlador.informativo("Se registro con exito");
 							limpiar();
 						} else {
@@ -90,7 +94,7 @@ public class ProveedorControlador implements Serializable {
 						UtilitarioControlador.advertencia("El RUC no es valido");
 					}
 				} else {
-					if (this.modprov.guardar(this.prov)) {
+					if (this.modcli.guardar(this.cli)) {
 						UtilitarioControlador.informativo("Se registro con exito");
 						limpiar();
 					} else {
@@ -104,8 +108,9 @@ public class ProveedorControlador implements Serializable {
 	}
 
 	/*
-	 * Getters y Setters
+	 * Getters Setters
 	 */
+
 	public String getBuscador() {
 		return buscador;
 	}
@@ -114,23 +119,23 @@ public class ProveedorControlador implements Serializable {
 		this.buscador = buscador;
 	}
 
-	public List<Proveedor> getLista() {
+	public List<Cliente> getLista() {
 		return lista;
 	}
 
-	public void setLista(List<Proveedor> lista) {
+	public void setLista(List<Cliente> lista) {
 		this.lista = lista;
 	}
 
-	public Proveedor getProv() {
-		return prov;
+	public Cliente getCli() {
+		return cli;
 	}
 
-	public void setProv(Proveedor prov) {
-		this.prov = prov;
+	public void setCli(Cliente cli) {
+		this.cli = cli;
 	}
 
-	public ProveedorControlador() {
+	public ClienteControlador() {
 		this.todo();
 	}
 }

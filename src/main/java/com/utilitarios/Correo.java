4a1cab1286b068af196a632bf2afdb67794c98d3
Eduardo Montenegro <eduardomontenegro599@gmail.com>
2021-08-12 16:48:16 -0500

    agregacion de funciones de correo y reportes

---
+++ src/main/java/com/utilitarios/Correo.java
@@ -0,0 +1,56 @@
+package com.utilitarios;
+
+import java.io.Serializable;
+import java.util.Properties;
+import javax.mail.Flags;
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+public class Correo implements Serializable {
+
+	private final static MimeMultipart multi = new MimeMultipart();
+	private final static MimeBodyPart cuerpo = new MimeBodyPart();
+	private final static MimeBodyPart adjunto = new MimeBodyPart();
+	private final static Properties props = new Properties();
+	private static Session sesion;
+	private static MimeMessage mensaje;
+	private static Transport trans;
+	
+	public static boolean correoCA(String destino,String asunto, String txt_mensaje, String archivo) {
+		try {
+			props.put("mail.smtp.host", "smtp.mail.yahoo.com");
+			props.put("mail.smtp.starttls.enable", "true");
+			props.put("mail.smtp.port", 587);
+			props.put("mail.smtp.mail.sender", "eduardopato1999afs@yahoo.com");
+			props.put("mail.smtp.user", "eduardopato1999afs@yahoo.com");
+			props.put("mail.smtp.auth", "true");
+			sesion = Session.getInstance(props);
+
+			cuerpo.setText(txt_mensaje);
+			adjunto.attachFile(archivo);
+			multi.addBodyPart(cuerpo);
+			multi.addBodyPart(adjunto);
+
+			mensaje= new MimeMessage(sesion);
+			mensaje.setFrom(new InternetAddress(props.getProperty("mail.smtp.mail.sender")));
+			mensaje.addRecipient(Message.RecipientType.TO, new InternetAddress(destino));
+			mensaje.setContent(multi);
+			mensaje.setSubject(asunto);
+			mensaje.setFlag(Flags.Flag.FLAGGED, true);
+
+			trans = sesion.getTransport("smtp");
+			trans.connect((String) props.getProperty("mail.smtp.user"), "kwugbzwllwqnouqf");
+			trans.sendMessage(mensaje, mensaje.getAllRecipients());
+			trans.close();
+			return true;
+		} catch (Exception e) {
+			System.out.println("ERROR AL ENVIAR EL CORREO: " + e);
+			return false;
+		}
+	}
+}
---
+++ src/main/java/com/utilitarios/Reportes.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.utilitarios;
+
+import com.modelo.Conexion;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.Serializable;
+import java.util.HashMap;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.util.JRLoader;
+
+/**
+ *
+ * @author panchito
+ */
+public class Reportes implements Serializable {
+
+	private static JasperReport reporte;
+	private static final HashMap<String, Object> props = new HashMap<>();
+	private static File archivo;
+	private static final String dire = "/home/reports/salida.pdf";
+
+	public static String generarCotizacion(int id) throws FileNotFoundException, JRException, NullPointerException {
+		Reportes.props.put("ven_id", id);
+		Reportes.archivo = new File("/home/reports/cotizacion.jasper");
+		if (!Reportes.archivo.exists()) {
+			throw new FileNotFoundException();
+		}
+		Reportes.reporte = (JasperReport) JRLoader.loadObject(Reportes.archivo);
+		JasperPrint fabrica = JasperFillManager.fillReport(Reportes.reporte, Reportes.props, Conexion.conectar());
+		JasperExportManager.exportReportToPdfFile(fabrica, Reportes.dire);
+		return Reportes.dire;
+	}
+}
